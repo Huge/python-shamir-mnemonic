f736c38078207b0729994730621bba95e8b79ebd
Huge ~ Martin <userHuge@gmail.com>
2020-12-16 19:51:45 +0100

    simplify the main to barebone, tested and agruments come through

--- main.py
+++ main.py
@@ -2,8 +2,5 @@
 """ From https://stackoverflow.com/a/16985066 to resolve `ImportError: attempted relative import with no known parent package` """
 from shamir_mnemonic.cli import cli
 
-def main():
-    cli()
-
 if __name__ == '__main__':
-    main()
+    cli()
